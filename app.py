f6321f4404b20d4d042c49907e9add28054f2b06
KOMATI RAKESH <rrocky87221@gmail.com>
2025-07-28 13:55:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+import streamlit as st
+from movie_recommender import get_recommendations
+
+# Title of the web app
+st.title("Movie Genre-Based Recommender")
+
+# User input for genre
+genre_input = st.text_input("Enter a Genre (e.g., Comedy, Horror, Action)")
+
+# Display recommendations
+if genre_input:
+    recommendations = get_recommendations(genre_input)
+    st.write(f"Top 5 {genre_input} Movies:")
+    for movie in recommendations:
+        st.write(movie)
